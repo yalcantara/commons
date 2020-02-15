85c805a2e3caa2c102dae8deefacb09c5f3a5cf5
Yaison <yaison1708@gmail.com>
2020-02-15 11:50:48 -0400

    Better support for http status codes.

--- include/commons/web/RestClient.h
+++ include/commons/web/RestClient.h
@@ -23,41 +23,84 @@ namespace web {
 
 class RestClient {
 private:
-	std::string root;
+	string root;
     http_client client;
 
-public:
+    string fullp(const char* path, Query& query){
+        uri_builder b;
+        b.append_path(path);
+        string q = query.to_string();
+        b.append_query(q);
 
+        string target = b.to_string();
+        return target;
+    }
+
+public:
 
 	RestClient(const char* root) :root(root), client(root) {
 	}
 
-	Response get(const char* target) {
-		http_response res = client.request(methods::GET, target).get();
 
-		uri_builder b{root};
-		b.append(target);
-		uri url = b.to_uri();
+	Response get(const char* path_query_fragment) {
+
+        //construct the url first, before doing any network call.
+        uri_builder b{root};
+        b.append(path_query_fragment);
+        uri url = b.to_uri();
+
+		http_response res = client.request(methods::GET, path_query_fragment).get();
+
 		Response ans{res, url};
 
 		return ans;
 	}
 
-	Response get(std::string& target) {
-		const char* str = target.c_str();
+	Response get(std::string& path_query_fragment) {
+		const char* str = path_query_fragment.c_str();
 		return get(str);
 	}
 
 	Response get(const char* path, Query& query) {
-		uri_builder b;
-		b.append_path(path);
-		string q = query.to_string();
-		b.append_query(q);
-
-		string target = b.to_string();
-
+		string target = fullp(path, query);
 		return get(target);
 	}
+
+    Response get2xx(const char* path_query_fragment){
+        Response r = get(path_query_fragment);
+        unsigned short status = r.status_code();
+        if(status < 200 || status >= 300){
+            string url = r.url().to_string();
+            string msg = "Response is not 2xx. Got: " + std::to_string(status) +", for url: " + url +".";
+            throw Exception(msg);
+        }
+        return r;
+    }
+
+    Response get2xx(const char* path, Query& q){
+	    string target = fullp(path, q);
+	    return get2xx(target.c_str());
+	}
+
+    string wget(const char* path, Query& q){
+        return get2xx(path, q).body_utf8();
+    }
+
+    string wget(const char* path){
+        return get2xx(path).body_utf8();
+    }
+
+    string wget(string& path){
+        return wget(path.c_str());
+    }
+
+    u16string wget_utf16(const char* path){
+        return get2xx(path).body_utf16();
+    }
+
+    u16string wget_utf16( const char* path, Query& q){
+        return get2xx(path, q).body_utf16();
+    }
 };
 
 } /* namespace core */
